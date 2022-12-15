bbf3691c0a3313948d9900687318a0f0d8a29295
Jérémy Brialon <jeremy.brialon@emea.merkleinc.com>
2022-12-15 09:11:24 +0100

    update video

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -6,11 +6,11 @@ export default {};
   <section id="video">
     <!-- <iframe src="https://player.vimeo.com/video/780765242" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe> -->
     <iframe
-      src="https://player.vimeo.com/video/780765242?h=d2461ed2fd&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+      src="https://player.vimeo.com/video/781182207?h=701aad0911"
       frameborder="0"
       allow="autoplay; fullscreen; picture-in-picture"
       allowfullscreen
-      title="Devenir Beta"
+      title="Devenir"
     ></iframe>
   </section>
 </template>
